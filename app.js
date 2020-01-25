a3a240ed66975235069931cd5e910beebbe06c98
GamingDome <46267853+GamingDome@users.noreply.github.com>
2020-01-25 13:31:45 +0100

    stable version

--- app.js
+++ app.js
@@ -2,29 +2,30 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
-let width = 500;
+let width = 750;
 let height = 500;
 
+let spelerSize = {}
+spelerSize.width = 25;
+spelerSize.height = 100;
+
 let speler1 = {}
-speler1.x = 100;
-speler1.y = 100;
-speler1.width = 25;
-speler1.height = 100;
+speler1.id = null;
+speler1.x = 20;
+speler1.y = 250;
 
 let speler2 = {}
-speler2.x = 100;
-speler2.y = 100;
-speler2.width = 25;
-speler2.height = 100;
-
+speler2.id = null;
+speler2.x = width - 40;
+speler2.y = 250;
 
 let ball = {}
 ball.x = 200;
 ball.y = 200;
 ball.radius = 10;
-ball.speed = 300;
-ball.velY = 0.01;
-ball.velX = 0.01;
+ball.speed = 30;
+ball.velY = -0.1;
+ball.velX = -0.1;
 
 ball.update = function() {
   for(let x = 0; x < this.speed; x++) {
@@ -39,25 +40,36 @@ ball.update = function() {
 
 ball.collisionCheck = function() {
 
-    if ((ball.x - ball.radius) <= 0) {
-      ball.x = width/2;
-      ball.y = height/2;
-    }
-    if ((ball.x - ball.radius) >= width) {
-      ball.x = width/2;
-      ball.y = height/2;
-    }
+  if ((ball.y - ball.radius) <= 0) {
+    ball.velY = Math.abs(ball.velY);
+  }
 
-    if ((ball.x - ball.radius) >= parseInt(speler1.x) && (ball.x - ball.radius) <= parseInt(speler1.x) + width) {
-      if ((ball.y - ball.radius) >= parseInt(speler1.y) && ((ball.y - ball.radius) <= parseInt(speler1.y) + height)) {
+  
+  if ((ball.y + ball.radius) >= height) {
+    ball.velY = -Math.abs(ball.velY);
+  }
+
+  // wanneer er gescoord word.  
+  if ((ball.x - ball.radius) <= 0 || (ball.x + ball.radius) >= width) {
+    ball.velY = -(ball.velY);
+    ball.velX = -(ball.velX);
+    ball.x = width/2;
+    ball.y = height/2;
+  }
+
+  // De collisions van de speler zelf.
+    if ((ball.x - ball.radius) >= parseInt(speler1.x) && (ball.x - ball.radius) <= parseInt(speler1.x) + spelerSize.width) {
+      if ((ball.y - ball.radius) >= parseInt(speler1.y) && ((ball.y - ball.radius) <= parseInt(speler1.y) + spelerSize.height)) {
         ball.velX = Math.abs(ball.velX);
         }
       }
-        if ((ball.x + ball.radius) >= parseInt(speler2.x) && (ball.x + ball.radius) <= parseInt(speler2.x) + width) {
-          if ((ball.y + ball.radius) >= parseInt(speler2.y) && ((ball.y - ball.radius) <= parseInt(speler2.y) + height)) {
-            ball.velX = -Math.abs(ball.velX);
-            }
-  }
+
+    if ((ball.x + ball.radius) >= parseInt(speler2.x) && (ball.x + ball.radius) <= parseInt(speler2.x) + spelerSize.width) {
+      if ((ball.y + ball.radius) >= parseInt(speler2.y) && ((ball.y + ball.radius) <= parseInt(speler2.y) + spelerSize.height)) {
+        ball.velX = -Math.abs(ball.velX);
+        }
+      }
+
 }
 
 app.get('/', function(req, res){
@@ -70,12 +82,27 @@ http.listen(3000, function(){
 });
 
 io.on('connection', function(socket){
+  if (speler1.id == null) speler1.id = socket.id;
+  else if (speler2.id == null) speler2.id = socket.id;
+
+  io.emit('setup', spelerSize);
+
+  socket.on("mousemove", function(y) {
+    if (speler1.id == socket.id) {
+      speler1.y = y;
+    }
+    if (speler2.id == socket.id) {
+      speler2.y = y;
+    }
+  });
+
+
   draw = function() {
     io.emit("clearScreen");
     io.emit("moveplayers", speler1, speler2);
     ball.update();
-    io.emit("moveball", ball);
+    io.emit("moveball", ball.x, ball.y, ball.radius);
   }
 
-  setInterval(draw, 10);
+  setInterval(draw, 100);
 });
